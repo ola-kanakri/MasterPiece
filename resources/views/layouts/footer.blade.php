bb04ab22bc5122f7607b032053c7cd812667e1d1
ola-kanakri <ola.kanakri12@gmail.com>
2022-08-13 22:50:41 +0300

    ola

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,56 @@
+<footer class="footer ">
+    <div class="footer_info">
+      <div class="row mb-5">
+        <div class="col-md-6 col-lg-4">
+          <h3 class="heading-section">Who we are</h3>
+          <p class="lead">CASH.COM is the number one Arabic classifieds platform in the Middle East with over 2 billion monthly pageviews. We connect buyers and sellers in real-time to be able to sell, buy, get a service or a job. 
+
+            </p>
+          <p><a href="/about" class="link-underline">Read more</a></p>
+        </div>
+        <div class="col-md-6 col-lg-4" >
+          <h3 class="heading-section" style="margin-left: 30px">About us</h3>
+
+          <div class="block-21 d-flex mb-4">
+
+            <div class="text" style="margin-left: 30px">
+              <h3 class="lead"><a href="#">Have a question?</a></h3>
+              <div class="meta">
+                <div><a href="#"><span class="icon-calendar"></span> July 29, 2022</a></div>
+                <div><a href="#"><span class="icon-person"></span> Admin</a></div>
+                <div><a href="#"><span class="icon-chat"></span> 19</a></div>
+              </div>
+            </div>
+          </div>
+
+
+        </div>
+        <div class="col-md-6 col-lg-4">
+          <div class="block-23">
+            <h3 class="heading-section">Keep in touch</h3>
+              <ul>
+
+                <li><a href="#"><span class="icon icon-phone"></span><span class="text">+2 392 3929 210</span></a></li>
+                <li><a href="#"><span class="icon icon-envelope"></span><span class="text">info@yourdomain.com</span></a></li>
+              </ul>
+
+            </div>
+        </div>
+
+
+      </div>
+
+        <div class="col-md-12 text-center">
+            <p>
+                <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This project is made by Ola Kanakri</a>
+                <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                </p>
+
+
+        </div>
+
+        <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF" crossorigin="anonymous"></script>
+
+  </footer>
---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,98 @@
+@extends('layouts.master')
+
+@section('content')
+
+
+  <div class="block-31" style="position: relative;">
+    <div class=" loop-block-31 ">
+      <div class="block-30 block-30-sm item" style="background-image: url('images/land.jpg') !important;" data-stellar-background-ratio="0.5">
+        <div class="container">
+          <div class="row align-items-center justify-content-center">
+            <div class="col-md-7 text-center">
+              <h2 class="heading">About Us</h2>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </div>
+
+
+      <div class="site-section">
+        <div class="container">
+          <div class="row align-items-center mb-5">
+
+            <div class="col-md-7 order-md-2 mb-5 mb-md-0">
+              <img src="images/grapes.jpg" alt="" class="img-fluid">
+            </div>
+
+            <div class="col-md-5 pr-md-5 mb-5 order-md-1">
+              <div class="block-41">
+                <div class="block-41-subheading d-flex">
+                  {{-- <div class="block-41-number">Giving Is Our Symbol</div> --}}
+                </div>
+                <h2 class="block-41-heading mb-3">Who we are?</h2>
+                <div class="block-41-text">
+                  <p>Cash.Com is the number one Arabic classifieds platform in the Middle East with over 2 billion monthly pageviews. We connect buyers and sellers in real-time to be able to sell, buy, get a service or a job. 
+
+                    See More</p>
+                </div>
+              </div>
+            </div>
+
+          </div> <!-- .row -->
+
+           <!-- .row -->
+
+        </div>
+      </div>
+
+
+    </div>
+  </div>
+
+  {{-- <div class="site-section border-top">
+    <div class="container">
+      <div class="row">
+
+        <div class="col-md-4">
+          <div class="media block-6">
+            <div class="icon"><span class="ion-ios-bulb"></span></div>
+            <div class="media-body">
+              <h3 class="heading">Our Mission</h3>
+              <p>A small river named Duden flows by their place and supplies it with the necessary regelialia.</p>
+              <p><a href="#" class="link-underline">Learn More</a></p>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-4">
+          <div class="media block-6">
+            <div class="icon"><span class="ion-ios-cash"></span></div>
+            <div class="media-body">
+              <h3 class="heading">Make Donations</h3>
+              <p>A small river named Duden flows by their place and supplies it with the necessary regelialia.</p>
+              <p><a href="#" class="link-underline">Learn More</a></p>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-4">
+          <div class="media block-6">
+            <div class="icon"><span class="ion-ios-contacts"></span></div>
+            <div class="media-body">
+              <h3 class="heading">We Need Volunteers</h3>
+              <p>A small river named Duden flows by their place and supplies it with the necessary regelialia.</p>
+              <p><a href="#" class="link-underline">Learn More</a></p>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </div> <!-- .site-section -->
+ --}}
+
+<!-- .site-section -->
+@endsection
